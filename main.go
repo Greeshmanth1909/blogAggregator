5254d43c91ef6aaeb6ab8339bf75942d18a50859
Greeshmanth1909 <91905738+Greeshmanth1909@users.noreply.github.com>
2024-09-16 12:17:00 +0530

    boilerplate for multiplexer and server

--- main.go
+++ main.go
@@ -1,7 +1,58 @@
 package main
 
-import "fmt"
-
+import (
+    "fmt"
+    "github.com/joho/godotenv"
+    "log"
+    "os"
+    "net/http"
+    "encoding/json"
+)
 func main() {
-    fmt.Println("Hello world")
+    err := godotenv.Load()
+    if err != nil {
+    log.Fatal("Error loading .env file")
+    }
+
+    port := os.Getenv("PORT")
+    fmt.Println(port)
+
+    mux := http.NewServeMux()
+
+    var server http.Server
+    server.Addr = "localhost:" + port
+    server.Handler = mux
+
+    mux.HandleFunc("GET /v1/healthz", healthHandler)
+    mux.HandleFunc("GET /v1/err", errHandler)
+
+    server.ListenAndServe()
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+    dat, err := json.Marshal(payload) 
+    if err != nil {
+        fmt.Println("something up with marshalling payload")
+    }
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(code)
+    w.Write(dat)
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+    body := struct{
+        Error string `json:"error"`
+    }{msg}
+    respondWithJSON(w, code, body)
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+    body := struct{
+        Status string `json:"status"`
+    }{"OK"}
+    respondWithJSON(w, 200, body)
+}
+
+func errHandler(w http.ResponseWriter, r *http.Request) {
+    respondWithError(w, 500, "Internal Server Error")
 }
